fd348928ae8306f23eef9d53b120798692114c72
santocaridi99 <santocaridi99@gmail.com>
2021-12-02 15:23:40 +0100

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 // qui chiedo di inserire nome con un prompt
-
-
+const userName = prompt('inserisci il tuo nome')
 //qui chiedo di inserire cognome con un prompt
-
-
+const userLastname = prompt('inserisci il tuo cognome')
 //qui chiedo di inserire colore preferito
-
-
+const favoriteColor = prompt('inserisci il tuo colore preferito')
+//qui inserisco numero 21
+let number = 21;
 //qui prenderò id pass nell'html e metterò dentro gli imput scritti sopra
+let htmlElement = document.getElementById('pass');
+htmlElement.innerHTML = `Password : ${userName}${userLastname}${favoriteColor}${number}`
