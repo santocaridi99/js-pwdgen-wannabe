e4c411f19ac70941a74f2a247e1778740a87943b
santocaridi99 <santocaridi99@gmail.com>
2021-12-02 15:11:38 +0100

    creazione file e aggiunta commenti

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// qui chiedo di inserire nome con un prompt
+
+
+//qui chiedo di inserire cognome con un prompt
+
+
+//qui chiedo di inserire colore preferito
+
+
+//qui prenderò id pass nell'html e metterò dentro gli imput scritti sopra
