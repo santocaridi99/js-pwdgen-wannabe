6f8f25f1c47a0c4d00d0c573b6d47fc4d8112be6
santocaridi99 <santocaridi99@gmail.com>
2021-12-02 16:29:25 +0100

    aggiunto esercizio "key" al mio bonus

--- bonus/js-bonus/script-bonus.js
+++ bonus/js-bonus/script-bonus.js
@@ -7,9 +7,25 @@ const secondWord = prompt('inserisci seconda parola a caso');
 console.log(secondWord);
 //qui inserisco numero
 let a = 1;
+//visualizzarlo in console
 console.log(a);
-
-
 //qui prenderò id pass nell'html e metterò dentro gli input scritti sopra
 let htmlElement = document.getElementById('password');
 htmlElement.innerHTML = `Password : ${firstWord}${secondWord}${a}`
+
+//inserisco due numeri interi e una stringa con scritta 22
+let number1 = 20;//intero
+let number2 = 2;//intero
+let number4 ='22';//stringa
+//test1 farà la somma tra il numero 1 e il numero 2
+let test1 = number1 + number2;
+//controllo test1 in console
+console.log(test1);
+//voglio che in html verrà scritto h1 rosso con prima parola + somma + numero4 stringa
+console.log(firstWord + test1 + number4);
+document.getElementById('key').innerHTML = `key : ${firstWord}${test1}${number4} `
+
+
+
+
+
